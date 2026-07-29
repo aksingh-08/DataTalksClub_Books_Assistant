6d38e9f4139398e3566d9d2127b7ffe74e578614
aksingh-08 <ashishk.singh1008@gmail.com>
2026-07-29 18:33:20 +0530

    update

--- app/sidebar.py
+++ app/sidebar.py
@@ -1,15 +1,22 @@
 import streamlit as st
-from rag.config import(MODEL_NAME, SEARCH_METHOD)
+from rag.config import MODEL_NAME, SEARCH_METHOD
 
 def render_sidebar():
     with st.sidebar:
         st.title('Books Assistant')
         st.divider()
         st.subheader('Configuration')
-        st.write(f'Model: {MODEL_NAME}')
-        st.write(f'Retriever: {SEARCH_METHOD}')
+        st.write(f"**Model:** {MODEL_NAME}")
+        st.write(f"**Retriever:** {SEARCH_METHOD}")
+        st.divider()
+        st.metric(
+            'Conversation',
+            len(st.session_state.messages) // 2
+        )
+        if st.button('Clear Chat'):
+            st.session_state.messages = []
+            st.rerun()
         st.divider()
         st.info(
-            'Ask questions about the DataTalksClub'
-            'Book of the Week archive.'
+            'Ask questions about the DataTalksClub Book of the Week archive.'
         )
