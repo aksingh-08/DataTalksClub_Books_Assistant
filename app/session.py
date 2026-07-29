f62cab65075860f7ab6fa104f8853ffdf8a5f325
aksingh-08 <ashishk.singh1008@gmail.com>
2026-07-29 18:33:14 +0530

    update

--- app/session.py
+++ app/session.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
 def initialize_session():
-    if 'history' not in st.session_state:
-        st.session_state.history = []
-    if 'current_response' not in st.session_state:
-        st.session_state.current_response = None
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
