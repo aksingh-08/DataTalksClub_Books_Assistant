454eebcf3774582f0464adefd05e684894d5a170
aksingh-08 <ashishk.singh1008@gmail.com>
2026-07-29 18:33:39 +0530

    update

--- rag/llm.py
+++ rag/llm.py
@@ -2,20 +2,24 @@ from pathlib import Path
 import os
 
 from dotenv import load_dotenv
-from openai import OpenAI
+# from openai import OpenAI
+from groq import Groq
 
-from rag.config import MODEL_NAME, BASE_URL, MAX_COMPLETION_TOKENS
+from rag.config import MODEL_NAME, MAX_COMPLETION_TOKENS
 
 load_dotenv()
-GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
-if not GITHUB_TOKEN:
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+if not GROQ_API_KEY:
     raise ValueError(
-        'GITHUB_TOKEN not found. Please set it in your .env file.'
+        'GROQ_API_KEY not found. Please set it in your .env file.'
     )
 
-client = OpenAI(
-    api_key=GITHUB_TOKEN,
-    base_url=BASE_URL
+# client = OpenAI(
+#     api_key=GITHUB_TOKEN,
+#     base_url=BASE_URL
+# )
+client = Groq(
+    api_key=GROQ_API_KEY,
 )
 
 def generate_answer(prompt: str):
