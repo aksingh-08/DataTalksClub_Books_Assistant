086855a9e95aeb6f5a602f3bec25eef2f10a7aa7
aksingh-08 <ashishk.singh1008@gmail.com>
2026-07-29 18:33:26 +0530

    update

--- app/styles.py
+++ app/styles.py
@@ -1,12 +1,13 @@
 import streamlit as st
+
 def load_css():
     st.markdown(
         '''
         <style>
         .block-container{
+            max-width:1100px;
             padding-top:2rem;
             padding-bottom:2rem;
-            max-width:1100px;
         }
         </style>
         ''',
